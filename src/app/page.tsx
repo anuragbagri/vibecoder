9ef7fa03a1b8bcb83aca603ddee748b0d84f739b
anurag bagri <anurag123bagri@gmail.com>
2025-10-18 09:12:47 +0530

    trpc data access layer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import { Suspense } from "react";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClient, trpc } from "@/trpc/server";
+import Client from "./client";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "anurag" })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>..loading</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 }
