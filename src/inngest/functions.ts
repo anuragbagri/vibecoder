e686468b5e88867338393563e9f3b3def062f017
anurag bagri <anurag123bagri@gmail.com>
2025-10-19 17:32:48 +0530

    Added e2b sandbox for running nextjs templates

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "@/inngest/client";
 import { createAgent, gemini } from '@inngest/agent-kit';
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("anurag-nextjs-2");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name : "codeAgent",
       system : "you are an expert nextjs coder. you read and write nextjs code and code snippets like react button component",
@@ -14,8 +20,12 @@ export const helloWorld = inngest.createFunction(
       `write the following snippet : ${event.data.value}`
     );
 
-    console.log(output)
+    const sandboxUrl = await step.run("get-sandbox-url" , async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host =  sandbox.getHost(3000);
+      return `https://${host}`
+    })
 
-    return { output };
+    return { output, sandboxUrl };
   },
 );
