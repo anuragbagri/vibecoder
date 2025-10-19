28e24a5811457157965f074d51ba6ebe5a94ca44
anurag bagri <anurag123bagri@gmail.com>
2025-10-19 10:08:55 +0530

    gemini flash model for testing

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,21 @@
 import { inngest } from "@/inngest/client";
+import { createAgent, gemini } from '@inngest/agent-kit';
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("transcribing the video", "5s");
+    const codeAgent = createAgent({
+      name : "codeAgent",
+      system : "you are an expert nextjs coder. you read and write nextjs code and code snippets like react button component",
+      model : gemini({model : "gemini-2.5-flash"}),
+    });
+    const { output } =await codeAgent.run(
+      `write the following snippet : ${event.data.value}`
+    );
 
-    // second step 
-    await step.sleep("ai summary and uplaod", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    console.log(output)
+
+    return { output };
   },
 );
